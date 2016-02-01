80de742127f38229cf0cc8f221dcf40e9004c92b
iadfeshchm <zlatov@ya.ru>
2016-02-02 00:53:56 +0300

    Модель Page, Проверка перед удалением beforeDelete, с генерацией своей ошибки

--- common/models/Page.php
+++ common/models/Page.php
@@ -23,78 +23,90 @@ use common\components\helpers\Text;
  */
 class Page extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return 'page';
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'page';
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['pid'], 'integer'],
-            [['header'], 'required'],
-            [['text'], 'string'],
-            [['order'], 'default', 'value'=>'100','when'=> function($model){ return $model->isNewRecord; }],
-            [['order'], 'integer', 'min' => 0, 'max' => 65535,],
-            [['header', 'desription', 'keywords'], 'string', 'max' => 255],
-            [['sid'], 'string', 'max' => 80],
-            [['title'], 'string', 'max' => 160],
-            [['sid'], 'unique'],
-            [['header'], 'unique']
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['pid'], 'integer'],
+			[['header'], 'required'],
+			[['text'], 'string'],
+			[['order'], 'default', 'value'=>'100','when'=> function($model){ return $model->isNewRecord; }],
+			[['order'], 'integer', 'min' => 0, 'max' => 65535,],
+			[['header', 'desription', 'keywords'], 'string', 'max' => 255],
+			[['sid'], 'string', 'max' => 80],
+			[['title'], 'string', 'max' => 160],
+			[['sid'], 'unique'],
+			[['header'], 'unique']
+		];
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'Ид. номер',
-            'pid' => 'Родительская страница',
-            'header' => 'Заголовок',
-            'sid' => 'Строковый ид.',
-            'text' => 'Текст',
-            'order' => 'Для сортировки',
-            'title' => 'Title',
-            'desription' => 'Description',
-            'keywords' => 'Keywords',
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => 'Ид. номер',
+			'pid' => 'Родительская страница',
+			'header' => 'Заголовок',
+			'sid' => 'Строковый ид.',
+			'text' => 'Текст',
+			'order' => 'Для сортировки',
+			'title' => 'Title',
+			'desription' => 'Description',
+			'keywords' => 'Keywords',
+		];
+	}
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getP()
-    {
-        return $this->hasOne(Page::className(), ['id' => 'pid']);
-    }
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getP()
+	{
+		return $this->hasOne(Page::className(), ['id' => 'pid']);
+	}
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getPages()
-    {
-        return $this->hasMany(Page::className(), ['pid' => 'id']);
-    }
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getPages()
+	{
+		return $this->hasMany(Page::className(), ['pid' => 'id']);
+	}
 
-    public function beforeSave($insert)
-    {
-        if (parent::beforeSave($insert)) {
-            $this->sid = Text::sid($this->sid,$this->header);
-            if ($this->sid === '') {
-                $this->addError('sid', 'Строковый идентификатор не смог создасться из заголовка, измените заголовок.');
+	public function beforeSave($insert)
+	{
+		if (parent::beforeSave($insert)) {
+			$this->sid = Text::sid($this->sid,$this->header);
+			if ($this->sid === '') {
+				$this->addError('sid', 'Строковый идентификатор не смог создасться из заголовка, измените заголовок.');
+				return false;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public function beforeDelete()
+	{
+		if (parent::beforeDelete()) {
+			if ((int)($this->getPages()->count())>0) {
+				$this->addError('id', 'У данной страницы есть потомки — сначало удалите потомков.');
                 return false;
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
