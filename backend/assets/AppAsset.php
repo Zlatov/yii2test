472c8d4fcca329021a2f08515b1d8a630a34d59f
iadfeshchm <zlatov@ya.ru>
2016-02-02 00:58:18 +0300

    Пример подключения файлов css js в AppAssets (в результате файлы подключил в самом виджете)

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -19,8 +19,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        // 'css/treeList.css',
     ];
     public $js = [
+        // 'js/treeList.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
