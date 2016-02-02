e0b4a1448d6dbad2c75113d41abddb083e933546
iadfeshchm <zlatov@ya.ru>
2016-02-03 01:43:19 +0300

    !!! Разобраться в неодключеном файле /page/css/treeList.css и ревертнуть этот коммит !!!

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -19,7 +19,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        // 'css/treeList.css',
+        'css/treeList.css',
     ];
     public $js = [
         // 'js/treeList.js',
