1ba115a95752e6f0ad620f7d9a1aa27e25fbad1e
iadfeshchm <zlatov@ya.ru>
2016-03-06 20:20:54 +0300

    Products - sql procedures

--- sql/procedures/procedures.sql
+++ sql/procedures/procedures.sql
@@ -3,8 +3,208 @@ use `webhobru_yii`;
 drop procedure if exists `news_count_in_sections`;
 drop procedure if exists `current_menu`;
 drop procedure if exists `main_menu`;
+drop procedure if exists `category_list`;
+drop procedure if exists `product_filters_fish`;
+drop procedure if exists `product_filters_quote`;
+drop procedure if exists `product_index`;
+drop procedure if exists `product_view_fish`;
+drop procedure if exists `product_view_quote`;
+drop procedure if exists `product_list_fish`;
+drop procedure if exists `product_list_quote`;
 
 delimiter ;;
+
+create procedure `product_filters_fish`()
+begin
+	select
+		min(`p`.`price`) as `price_min`,
+		max(`p`.`price`) as `price_max`
+	from
+		`product` `p`
+	inner join `fish` `f` on `f`.`product_id` = `p`.`id`;
+end;;
+
+create procedure `product_filters_quote`()
+begin
+	select
+		min(`p`.`price`) as `price_min`,
+		max(`p`.`price`) as `price_max`
+	from
+		`product` `p`
+	inner join `quote` `q` on `q`.`product_id` = `p`.`id`;
+end;;
+
+create procedure `category_list`()
+begin
+	select
+		`c`.`id`,
+		`c`.`sid`,
+		`c`.`header`
+	from
+		`category` `c`;
+end;;
+
+create procedure `product_list_quote`(
+	in param_offset int,
+    in param_rows int,
+    in param_from decimal(8,2),
+    in param_to decimal(8,2),
+    in param_html tinyint,
+    in param_q varchar(32)
+)
+begin
+	select SQL_CALC_FOUND_ROWS
+		`p`.`id`,
+		`p`.`header`,
+		`p`.`price`,
+		`c`.`id` as `category_id`,
+		`c`.`sid` as `category_sid`,
+		`c`.`header` as `category_header`,
+		`q`.`html` as `quote_html`,
+		`a`.`name` as `author_name`
+	from
+		`product` `p`
+		left join `category` `c` on `c`.`id` = `p`.`category_id`
+		inner join `quote` `q` on `q`.`product_id` = `p`.`id`
+        left join `author` `a` on `a`.`id` = `q`.`author_id`
+	where
+		`p`.`price` >= param_from
+        and `p`.`price` <= param_to
+        and if (param_html < 2, `q`.`html` = param_html, 1)
+        and if (char_length(param_q)>2, `q`.`content` like concat('%',param_q,'%'), 1)
+	limit param_offset, param_rows;
+end;;
+
+create procedure `product_list_fish`(
+	in param_offset int,
+	in param_rows int,
+    in param_from decimal(8,2),
+    in param_to decimal(8,2),
+    in param_html tinyint,
+    in param_strong tinyint,
+    in param_em tinyint,
+    in param_q varchar(32)
+)
+begin
+	select SQL_CALC_FOUND_ROWS
+		`p`.`id`,
+		`p`.`header`,
+		`p`.`price`,
+		`c`.`id` as `category_id`,
+		`c`.`sid` as `category_sid`,
+		`c`.`header` as `category_header`,
+		`f`.`html` as `fish_html`,
+		`f`.`strong` as `fish_strong`,
+		`f`.`em` as `fish_em`
+	from
+		`product` `p`
+		left join `category` `c` on `c`.`id` = `p`.`category_id`
+		inner join `fish` `f` on `f`.`product_id` = `p`.`id`
+	where
+		`p`.`price` >= param_from
+        and `p`.`price` <= param_to
+        and if (param_html < 2, `f`.`html` = param_html, 1)
+        and if (param_strong < 2, `f`.`strong` = param_strong, 1)
+        and if (param_em < 2, `f`.`em` = param_em, 1)
+        and if (char_length(param_q)>2, `f`.`content` like concat('%',param_q,'%'), 1)
+	limit param_offset, param_rows;
+end;;
+
+create procedure `product_view_fish`(in param_id int)
+begin
+	select 
+		`p`.`id`,
+		`p`.`header`,
+		`p`.`price`,
+		`c`.`id` as `category_id`,
+		`c`.`sid` as `category_sid`,
+		`c`.`header` as `category_header`,
+		`f`.`paragraph` as `fish_paragraph`,
+		`f`.`html` as `fish_html`,
+		`f`.`strong` as `fish_strong`,
+		`f`.`em` as `fish_em`,
+		`f`.`content` as `fish_content`
+	from
+		`product` `p`
+		left join `category` `c` on `c`.`id` = `p`.`category_id`
+		left join `fish` `f` on `f`.`product_id` = `p`.`id`
+	where
+		`p`.`id` = param_id;
+end;;
+
+create procedure `product_view_quote`(in param_id int)
+begin
+	select 
+		`p`.`id`,
+		`p`.`header`,
+		`p`.`price`,
+		`c`.`id` as `category_id`,
+		`c`.`sid` as `category_sid`,
+		`c`.`header` as `category_header`,
+		`q`.`html` as `quote_html`,
+		`q`.`content` as `quote_content`,
+		`a`.`name` as `author_name`
+	from
+		`product` `p`
+		left join `category` `c` on `c`.`id` = `p`.`category_id`
+		left join `quote` `q` on `q`.`product_id` = `p`.`id`
+		left join `author` `a` on `a`.`id` = `q`.`author_id`
+	where
+		`p`.`id` = param_id;
+end;;
+
+create procedure `product_index`()
+begin
+	(
+		select 
+			`p`.`id`,
+			`p`.`header`,
+			`p`.`price`,
+			`c`.`sid` as `category_sid`,
+			`c`.`header` as `category_header`,
+			`f`.`paragraph`,
+			coalesce(`f`.`html`,`q`.`html`) as `html`,
+			`f`.`strong` as `strong`,
+			`f`.`em` as `em`,
+			`a`.`name` as `author_name`
+		from
+			`product` `p`
+			left join `category` `c` on `c`.`id` = `p`.`category_id`
+			left join `fish` `f` on `f`.`product_id` = `p`.`id`
+			left join `quote` `q` on `q`.`product_id` = `p`.`id`
+			left join `author` `a` on `a`.`id` = `q`.`author_id`
+		where
+			p.category_id = 1
+		order by
+			`p`.`id` desc
+		limit 0, 3
+    )
+union
+	(
+		select 
+			`p`.`id`,
+			`p`.`header`,
+			`p`.`price`,
+			`c`.`sid` as `category_sid`,
+			`c`.`header` as `category_header`,
+			`f`.`paragraph`,
+			coalesce(`f`.`html`,`q`.`html`) as `html`,
+			`f`.`strong` as `strong`,
+			`f`.`em` as `em`,
+			`a`.`name` as `author_name`
+		from
+			`product` `p`
+			left join `category` `c` on `c`.`id` = `p`.`category_id`
+			left join `fish` `f` on `f`.`product_id` = `p`.`id`
+			left join `quote` `q` on `q`.`product_id` = `p`.`id`
+			left join `author` `a` on `a`.`id` = `q`.`author_id`
+		where
+			p.category_id = 2
+		order by
+			`p`.`id` desc
+		limit 0, 3
+	);
+end ;;
 
 create procedure `news_count_in_sections`()
 begin
