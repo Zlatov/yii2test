87abd635253677833c36f279969e3f48a6b137e0
iadfeshchm <zlatov@ya.ru>
2016-03-19 17:27:49 +0300

    test Pjax (see Pjax with in form in basket in previous commit)

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -1,7 +1,7 @@
 <?php
 
 /* @var $this yii\web\View */
-
+use yii\widgets\Pjax;
 use yii\helpers\Html;
 
 $this->title = 'About';
@@ -11,7 +11,15 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Что-то там в этом файле ;) :</p>
-
+    <h2>Тест Pjax</h2>
+	<?php Pjax::begin(); ?>
+	    <?= Html::a(
+	        'Обновить',
+	        ['site/about'],
+	        ['class' => 'btn btn-lg btn-primary']
+	    ) ?>
+	    <p>Время сервера: <?= $time ?></p>
+	<?php Pjax::end(); ?>
     <p><code><?= __FILE__ ?></code></p>
     
     <pre><?= print_r($identity) ?></pre>
